131bed2a595fff272490a456cbce1645b18981db
shyfock <agvanegas@gmail.com>
2022-05-10 15:16:51 -0500

    First commit

---
+++ src/Package/NewClass.java
@@ -0,0 +1,34 @@
+package Package;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+
+        // Tipos de datos en java (primitivos)
+
+        byte tipoDato1 = 123;
+        int tipoDato2 = 1234567890;
+        short tipoDato3 = 12345;
+        long tipoDato4 = 1234567890;
+
+        float tipoDato5 = 1.123456f;
+        double tipoDato6 = 1.123456789d;
+
+        boolean tipoDato7 = true;
+
+        char tipoDato8 = 'a';
+        String tipoDato9 = "aA bB cC, Cadenas de texto";
+
+        System.out.println("Tipo de dato          Ejemplo \n +++++++++++++++++++++++++");
+        System.out.println("byte:                   " + tipoDato1);
+        System.out.println("int:                    " + tipoDato2);
+        System.out.println("short:                  " + tipoDato3);
+        System.out.println("long:                   " + tipoDato4);
+        System.out.println("float:                  " + tipoDato5);
+        System.out.println("double:                 " + tipoDato6);
+        System.out.println("boolean:                " + tipoDato7);
+        System.out.println("char:                   " + tipoDato8);
+        System.out.println("String:                 " + tipoDato9);
+    }
+
+}
